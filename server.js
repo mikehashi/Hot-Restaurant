347e493ff604477f1113d4d27b65105a8c913164
Donny Gabai <mrdgabai@gmail.com>
2017-08-05 11:21:27 -0400

    correct file names

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ var waitingList = [{
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 app.get("/input", function(req, res) {
-  res.sendFile(path.join(__dirname, "input.html"));
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 //listener
